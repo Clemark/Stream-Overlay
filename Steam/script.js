9212892698b404864e15db60b321fed89421ad4f
Clemark <clemarkcadapan@gmail.com>
2026-04-06 20:26:47 +0800

    Added Steam-ish Notification

---
+++ Steam/script.js
@@ -0,0 +1,47 @@
+////////////////////
+// CONFIG
+////////////////////
+const client = new StreamerbotClient();
+const sound = new Audio("audio/Steam.wav");
+
+////////////////////
+// FUNCTIONS
+////////////////////
+
+async function init() {
+  await client.on("General.Custom", (payload) => {
+    if (payload.data.event === "attendanceRedeem") {
+      let username = payload.data.username;
+      let pfpurl = payload.data.pfpurl;
+      let gamecategory = payload.data.category;
+      console.log(payload);
+
+      steamOnline(username, pfpurl, gamecategory);
+    }
+  });
+}
+
+init();
+
+function steamOnline(username, pfpurl, gamecategory) {
+  let pfpdiv = document.querySelector(".profile-picture");
+  pfpdiv.src = pfpurl;
+  let name = document.querySelector(".username");
+  let category = document.querySelector(".category");
+  name.innerHTML = username; // Set the username
+  category.innerHTML = gamecategory; // Set the category
+
+  triggerSlide();
+}
+
+function triggerSlide() {
+  const element = document.getElementById("steam-container");
+  element.classList.toggle("active");
+
+  sound.play();
+
+  // hide after 3 seconds
+  setTimeout(() => {
+    element.classList.remove("active");
+  }, 3000);
+}
